8e67744356b66825c821c8521ef7cf5a3eabb518
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 15:33:00 +0500

    Dz

--- PP2 Lab 5/5.1/5.1.2.py
+++ PP2 Lab 5/5.1/5.1.2.py
@@ -4,8 +4,8 @@ def match_a_followed_by_two_to_three_b(string):
     pattern = r'ab{2,3}'
     return bool(re.fullmatch(pattern, string))
 
-# Test examples
-print(match_a_followed_by_two_to_three_b("abb"))    # True
-print(match_a_followed_by_two_to_three_b("abbb"))   # True
-print(match_a_followed_by_two_to_three_b("abbbb"))  # False
-print(match_a_followed_by_two_to_three_b("ab"))     # False
+
+print(match_a_followed_by_two_to_three_b("abb"))    
+print(match_a_followed_by_two_to_three_b("abbb"))   
+print(match_a_followed_by_two_to_three_b("abbbb"))  
+print(match_a_followed_by_two_to_three_b("ab"))     
