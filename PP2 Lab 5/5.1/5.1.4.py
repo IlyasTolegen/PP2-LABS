1eac332cab875273594ada2cb52a19d85251e607
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 16:28:42 +0500

    Dz

--- PP2 Lab 5/5.1/5.1.4.py
+++ PP2 Lab 5/5.1/5.1.4.py
@@ -4,6 +4,5 @@ def find_uppercase_followed_by_lowercase(string):
     pattern = r'[A-Z][a-z]+'
     return re.findall(pattern, string)
 
-# Test examples
-print(find_uppercase_followed_by_lowercase("ThisIsAString"))  # ['This', 'Is', 'A']
-print(find_uppercase_followed_by_lowercase("CamelCaseWord"))  # ['Camel', 'Case', 'Word']
+print(find_uppercase_followed_by_lowercase("ThisIsAString"))  
+print(find_uppercase_followed_by_lowercase("CamelCaseWord")) 
