8141323f9306053a55d03401a9deffdf5dcf8b17
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 16:51:46 +0500

    Dz

--- PP2 Lab 5/5.1/5.1.6.py
+++ PP2 Lab 5/5.1/5.1.6.py
@@ -4,5 +4,5 @@ def replace_space_comma_dot_with_colon(string):
     pattern = r'[ ,.]'
     return re.sub(pattern, ':', string)
 
-# Test examples
-print(replace_space_comma_dot_with_colon("Hello, world. How are you?"))  # "Hello:world:How:are:you?"
+
+print(replace_space_comma_dot_with_colon("Hello, world. How are you?"))
