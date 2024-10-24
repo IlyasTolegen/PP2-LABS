cd812216df938106172ae350c98941480df4f3aa
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 15:40:38 +0500

    Dz

--- PP2 Lab6/6.1/6.1.1.py
+++ PP2 Lab6/6.1/6.1.1.py
@@ -2,8 +2,11 @@ from functools import reduce
 
 def multiply_list(numbers):
     return reduce(lambda x, y: x * y, numbers)
+input_numbers = input("Enter numbers separated by spaces: ")
 
-# Example usage
-numbers = [1, 2, 3, 4]
+numbers = list(map(int, input_numbers.split()))
+
 result = multiply_list(numbers)
+
 print(f"The product of the list is: {result}")
+
