fde330b3d02d25073206de9760dfbf80b6256db9
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 15:03:52 +0500

    5.1.10.py

--- PP2 Lab 5/5.1/5.1.10.py
+++ PP2 Lab 5/5.1/5.1.10.py
@@ -3,5 +3,4 @@ import re
 def camel_to_snake_case(camel_str):
     return re.sub(r'(?<!^)(?=[A-Z])', '_', camel_str).lower()
 
-# Test examples
-print(camel_to_snake_case("thisIsCamelCase"))  # "this_is_camel_case"
+print(camel_to_snake_case("thisIsCamelCase")) 
