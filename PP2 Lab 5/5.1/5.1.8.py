a23c06f777f02fe290b791c89560fb796746585c
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 16:52:53 +0500

    Dz

--- PP2 Lab 5/5.1/5.1.8.py
+++ PP2 Lab 5/5.1/5.1.8.py
@@ -3,5 +3,4 @@ import re
 def split_at_uppercase(string):
     return re.findall(r'[A-Z][^A-Z]*', string)
 
-# Test examples
-print(split_at_uppercase("ThisIsAString"))  # ['This', 'Is', 'A', 'String']
+print(split_at_uppercase("ThisIsAString"))
