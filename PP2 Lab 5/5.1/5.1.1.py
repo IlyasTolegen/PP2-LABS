931435183f0366e88d5142c51ff19b79cf6aa29f
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 16:43:37 +0500

    Dz

--- PP2 Lab 5/5.1/5.1.1.py
+++ PP2 Lab 5/5.1/5.1.1.py
@@ -3,8 +3,5 @@ import re
 def match_a_followed_by_b(string):
     pattern = r'ab*'
     return bool(re.fullmatch(pattern, string))
-
-print(match_a_followed_by_b("ab"))   
-print(match_a_followed_by_b("a"))     
-print(match_a_followed_by_b("abb"))   
-print(match_a_followed_by_b("ac"))    
+name=str(input("Enter the name: "))
+print(match_a_followed_by_b(name))   
