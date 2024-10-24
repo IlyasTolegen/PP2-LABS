0c60b12e57b4755852e8fb8cac72af3c13b1ca61
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 16:53:29 +0500

    Dz

--- PP2 Lab 5/5.1/5.1.9.py
+++ PP2 Lab 5/5.1/5.1.9.py
@@ -3,5 +3,4 @@ import re
 def insert_spaces_between_capitals(string):
     return re.sub(r'(?<!^)(?=[A-Z])', ' ', string)
 
-# Test examples
-print(insert_spaces_between_capitals("ThisIsAString"))  # "This Is A String"
+print(insert_spaces_between_capitals("ThisIsAString"))  
