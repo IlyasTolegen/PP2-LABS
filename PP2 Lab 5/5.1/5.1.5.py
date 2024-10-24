cbbc8d8851c5e82c687b9a7c5605eae8dd68afef
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 16:51:15 +0500

    Dz

--- PP2 Lab 5/5.1/5.1.5.py
+++ PP2 Lab 5/5.1/5.1.5.py
@@ -4,8 +4,8 @@ def match_a_followed_by_anything_ending_in_b(string):
     pattern = r'a.*b$'
     return bool(re.fullmatch(pattern, string))
 
-# Test examples
-print(match_a_followed_by_anything_ending_in_b("ab"))      # True
-print(match_a_followed_by_anything_ending_in_b("a123b"))   # True
-print(match_a_followed_by_anything_ending_in_b("a"))       # False
+
+print(match_a_followed_by_anything_ending_in_b("ab"))     
+print(match_a_followed_by_anything_ending_in_b("a123b"))  
+print(match_a_followed_by_anything_ending_in_b("a"))     
 
