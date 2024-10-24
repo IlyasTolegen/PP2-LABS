a844d47277671e3b609be583267b7d00d0daaf9e
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2024-10-24 16:28:10 +0500

    Dz

--- PP2 Lab 5/5.1/5.1.3.py
+++ PP2 Lab 5/5.1/5.1.3.py
@@ -4,6 +4,5 @@ def find_sequences_with_underscore(string):
     pattern = r'[a-z]+_[a-z]+'
     return re.findall(pattern, string)
 
-# Test examples
-print(find_sequences_with_underscore("lower_case_example"))  # ['lower_case']
-print(find_sequences_with_underscore("some_test_case"))      # ['some_test']
+print(find_sequences_with_underscore("lower_case_example"))  
+print(find_sequences_with_underscore("some_test_case"))     
